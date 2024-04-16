ef4ad901faa7d42887fcbd39c45cda84dcda171b
natch <raphael.gonon18@gmail.com>
2024-04-16 17:20:48 +0200

    action_tourner_case: check for pa

--- src/action_tourner_case.cc
+++ src/action_tourner_case.cc
@@ -5,9 +5,22 @@
 
 int ActionTournerCase::check(const GameState& st) const
 {
-    Carte carte = st.carte;
-    if (!carte.case_valide(pos_.colonne, pos_.ligne))
+    if (!st.init)
+        return HORS_TOUR;
+
+    Carte& carte = st.carte;
+    int x, y;
+    std::tie(x, y) = pos_
+    if (!carte.case_valide(x, y))
         return POSITION_INVALIDE;
+
+    Joueur& joueur = st.joueurs[player_id_];
+    Joueur& adversaire = st.joueurs[player_id_ ^ 1];
+    std::vector<std::vector<bool>> territoire_adverse = adversaire.territoire();
+    int cout = territoire_adverse[y][x] ? 2 : 1;
+    if (joueur.points_action < cout)
+        return PLUS_DE_PA;
+
     return OK;
 }
 
@@ -32,6 +45,13 @@ void ActionTournerCase::apply_on(GameState* st) const
 {
     int x = pos_.colonne;
     int y = pos_.lignt;
+
+    Joueur& joueur = st.joueurs[player_id_];
+    Joueur& adversaire = st.joueurs[player_id_ ^ 1];
+    std::vector<std::vector<bool>> territoire_adverse = adversaire.territoire();
+    int cout = territoire_adverse[y][x] ? 2 : 1;
+    joueur.points_action -= cout;
+
     enum type_case actuel = st.carte.get_case(x, y);
     st.carte.set_case(x, y, rotation_case(actuel));
 }
