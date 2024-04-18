eaf89b4ff629eeaed2f0adcb68e75a429d12104e
Guscraftin <augustin.glorian@outlook.com>
2024-04-18 17:34:06 +0200

    chore: remove useless fixme in action_debug_poser_drakkar

--- src/action_debug_poser_drakkar.cc
+++ src/action_debug_poser_drakkar.cc
@@ -5,13 +5,12 @@
 
 int ActionDebugPoserDrakkar::check(const GameState& st) const
 {
-    // FIXME: Need to check if it's your turn? PA?
     if (!st.carte.case_valide(pos_.ligne, pos_.colonne))
         return POSITION_INVALIDE;
 
     if (!(drakkar_ == PAS_DE_DRAKKAR || drakkar_ == DRAKKAR_BLEU ||
         drakkar_ == DRAKKAR_JAUNE || drakkar_ == DRAKKAR_ROUGE))
-        return CASE_INVALIDE; // FIXME : What return type?
+        return CASE_INVALIDE;
 
     return OK;
 }
