f18173d3c0a08824bcead710d81b78fbf5706952
oscar <oscar.cheevalier@epita.fr>
2024-04-19 12:17:59 +0200

    add_test_map: add case tests

--- src/carte.hh
+++ src/carte.hh
@@ -8,14 +8,13 @@
 class Carte
 {
 public:
-    // TODO: demander a un ACU
     Carte(const std::vector<std::string>& texte, const std::vector<std::vector<int>>& gain);
     Carte() = default;
     Carte(const Carte& carte) = default;
     Carte& operator=(const Carte& carte) = default;
     ~Carte() = default;
 
-    std::pair<int, int> get_dimension() const;
+    std::pair<int, int> get_dimension() const; // largeur_, hauteur_
     type_case get_case(int x, int y) const;
     void set_case(int x, int y, type_case nouvelle_case);
     int get_gain(int x, int y) const;
--- src/tests/test-carte.cc
+++ src/tests/test-carte.cc
@@ -7,10 +7,42 @@
 #include <sstream>
 #include <filesystem>
 
-TEST(CarteTest, txt_test)
+TEST(CarteTest, json_test_dimensions)
 {
     std::ifstream is("colibri.json");
-std::cout << std::filesystem::current_path() << std::endl;
     rules::Players players;
     GameState st(players, is);
+    Carte carte = st.carte;
+
+    ASSERT_EQ(carte.get_dimension().first, 5);
+    ASSERT_EQ(carte.get_dimension().second, 4);
+}
+
+TEST(CarteTest, json_test_cases)
+{
+    std::ifstream is("colibri.json");
+    rules::Players players;
+    GameState st(players, is);
+    Carte carte = st.carte;
+
+    // Tous les types de cases
+    ASSERT_EQ(carte.get_case(0, 0), SUD_EST);
+    ASSERT_EQ(carte.get_case(1, 0), SUD_OUEST);
+    ASSERT_EQ(carte.get_case(3, 0), NORD_OUEST);
+    ASSERT_EQ(carte.get_case(4, 0), VILLAGE_NEUTRE); //TODO modifier apres le changement dans le yaml
+    ASSERT_EQ(carte.get_case(1, 2), NORD_EST);
+
+    // Case bord
+    ASSERT_EQ(carte.get_case(1, 3), SUD_EST);
+
+    // Case coin
+    ASSERT_EQ(carte.get_case(4, 3), SUD_OUEST);
+
+    // Case invalide
+    ASSERT_EQ(carte.get_case(0, -1), CASE_INVALIDE);
+    ASSERT_EQ(carte.get_case(-42, -1), CASE_INVALIDE);
+    ASSERT_EQ(carte.get_case(-1, 0), CASE_INVALIDE);
+    ASSERT_EQ(carte.get_case(0, 4), CASE_INVALIDE);
+    ASSERT_EQ(carte.get_case(5, 0), CASE_INVALIDE);
+    ASSERT_EQ(carte.get_case(5, 4), CASE_INVALIDE);
 }
