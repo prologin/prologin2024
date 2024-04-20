37c732138a52d95f282f76c62e30c938c68b64d2
Celian RAIMBAULT <celian.raimbault@epita.fr>
2024-04-20 15:44:47 +0200

    front(prototype/editor): Added

---
+++ gui/godot/Scripts/Game/Models.gd
@@ -0,0 +1,92 @@
+class_name Models
+
+extends Node
+
+# --- See doc/format_carte_dump.md ---
+
+class Player:
+	var identifiant : int = 0
+	var scores
+	var actions
+
+
+class Aigle:
+	 var identifiant
+	 var joueur
+	 var pos
+	 var effet
+	 var puissance
+	 var tour_eclosion
+	 var existant
+
+
+class Map:
+	var width
+	var height
+	var carte = []
+	var points = []
+	var debug = []
+	var joueurs = []
+	var aigles = []
+	
+	func clear():
+		carte.clear()
+		points.clear()
+		debug.clear()
+		joueurs.clear()
+		aigles.clear()
+		
+	func init(width, height):
+		self.width = width
+		self.height = height
+
+		self.clear()
+
+		# Cases + score
+		for _i in range(height):
+			var row = []
+			var points_row = []
+			for _j in range(width):
+				row.append(Constants.TypeCase.NORD_OUEST)
+				if _j < width - 1:
+					points_row.append(1)
+			self.carte.append(row)
+			if _i < height - 1:
+				self.points.append(points_row)
+
+		# Villages
+		# TODO
+		carte[0][0] = Constants.TypeCase.VILLAGE # _J1
+		carte[-1][-1] = Constants.TypeCase.VILLAGE # _J2
+
+		# Players
+		var j1 = Player.new()
+		j1.identifiant = 1
+		var j2 = Player.new()
+		j2.identifiant = 2
+		self.joueurs = [j1, j2]
+
+	
+	# TODO
+	# func copy() -> models.Map:
+	#	var map := Map.new()
+	#	...
+	#	return map
+
+
+class Action:
+	var action_type
+	var debut
+	var fin
+	var identifiant_aigle
+
+
+class Tour:
+	var joueur_actuel
+	var id_tour
+	var fin
+
+
+class GameState:
+	var map : Map = null
+	var tour : Tour = null
---
+++ gui/godot/Scripts/Scenes/MapEditor/MapEditor.gd
@@ -0,0 +1,34 @@
+extends Node2D
+
+onready var viewer : Viewer = $HBoxContainer/ViewportContainer/Viewport/Viewer
+
+var bg_selection = null
+
+
+func _ready():
+	_on_ClearMap_pressed()
+
+
+# --- Click ---
+func _on_Viewer_bg_left_click(pos):
+	# TODO : Verif inside viewport
+	if bg_selection != null:
+		viewer.map.carte[pos[1]][pos[0]] = bg_selection
+	viewer.update_all(viewer.map)
+
+
+# --- Selector ---
+func _on_SampleMap_pressed():
+	var map = Models.Map.new()
+	viewer.update_all(map)
+
+
+func _on_ClearMap_pressed():
+	var map = Models.Map.new()
+	map.init(20, 10)
+	viewer.update_all(map)
+
+
+func _on_BGSelector_on_selection(tile):
+	bg_selection = viewer.tile2case[tile]
+	print('bg_selection: ', bg_selection)
