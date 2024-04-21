8676fbdaddd417fee42f2cb7c8bf1d62eaaa7a27
oscar <oscar.cheevalier@epita.fr>
2024-04-21 11:35:22 +0200

    action_deplacer_aigle: add PasAigle test

--- src/tests/test-action-deplacer-aigle.cc
+++ src/tests/test-action-deplacer-aigle.cc
@@ -2,7 +2,12 @@
 
 #include "../game_state.hh"
 
-TEST_F(ApiTest, ActionDeplacerAigleTest)
+TEST_F(ApiTest, ActionDeplacerAiglePasAigle)
 {
     const auto& st = api->game_state();
+    ASSERT_EQ(api->deplacer_aigle(0, position{0, 0}), AIGLE_INVALIDE);
+    ASSERT_EQ(st.aigles_sauvages[0].pos.colonne, 2);
+    ASSERT_EQ(st.aigles_sauvages[0].pos.ligne, 1);
 }
+
+// TODO YAKA
