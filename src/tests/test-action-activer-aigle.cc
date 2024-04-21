8068a1a3c5604026c20c148a6e3e1e70b8b2a6ee
oscar <oscar.cheevalier@epita.fr>
2024-04-21 13:21:50 +0200

    action_deplacer_aigle: add PasAigle test for activer-aigle

---
+++ src/tests/test-action-activer-aigle.cc
@@ -0,0 +1,9 @@
+#include "test-helpers.hh"
+
+TEST_F(ApiTest, ActionActiverAiglePasAigle)
+{
+    const auto& st = api->game_state();
+    ASSERT_EQ(api->activer_aigle(0), AIGLE_INVALIDE);
+    ASSERT_EQ(st.aigles_sauvages[0].pos.colonne, 2);
+    ASSERT_EQ(st.aigles_sauvages[0].pos.ligne, 1);
+}
