5e5ea80a04ff39ff9dad59108616762fd554f132
Rostan <rostan.tabet@gmail.com>
2024-05-26 20:11:21 +0000

    Add tooltips on the eggs
    Squashed commit of the following:
    
    * front(editor): lower the tooltip delay
    
    Signed-off-by: Rostan Tabet <rostan.tabet@gmail.com>
    
    * front(editor): add tooltips on the eggs
    
    Signed-off-by: Rostan Tabet <rostan.tabet@gmail.com>

--- gui/godot/Scripts/Scenes/MapEditor/BGSelector.gd
+++ gui/godot/Scripts/Scenes/MapEditor/BGSelector.gd
@@ -18,10 +18,35 @@ func _ready():
 		[Constants.TypeCase.OEUF_VERT, Constants.TypeCase.OEUF_BLEU, Constants.TypeCase.OEUF_ROUGE],
 		[Constants.TypeCase.OEUF_JAUNE, Constants.TypeCase.OEUF_GRIS],
 	]
+	var labels = {
+		Constants.TypeCase.OEUF_VERT: "Aigle de vie",
+		Constants.TypeCase.OEUF_BLEU: "Aigle de gel",
+		Constants.TypeCase.OEUF_ROUGE: "Aigle de feu",
+		Constants.TypeCase.OEUF_JAUNE: "Aigle de météore",
+		Constants.TypeCase.OEUF_GRIS: "Aigle de mort",
+	}
+	
+	var size = Vector2(110, 110)
 	for y in len(items): 
 		var row = items[y]
 		for x in len(row):
-			set_cell(x, y, viewer.case2tile[row[x]])
+			var cell = row[x]
+			set_cell(x, y, viewer.case2tile[cell])
+			if labels.has(cell):
+				var label = labels.get(cell)
+				add_collision_body(viewer, Vector2(x, y), size, label)
+
+func add_collision_body(node2D: Node2D, pos: Vector2, size: Vector2, label: String):
+	var rect = ColorRect.new()
+	var world_pos = map_to_world(pos)
+	rect.rect_position = world_pos
+	rect.color = Color(0.0, 0.0, 0.0, 0.0)
+	rect.rect_size = size
+	
+	rect.set_theme(get_parent().theme)
+	rect.set_tooltip(label)
+	
+	add_child(rect)
 
 func set_selection_rect(tilepos):
 	var pos : Vector2 = to_global(map_to_world(Vector2(tilepos[0], tilepos[1])))
