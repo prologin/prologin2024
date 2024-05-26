4e34d0720987eabca901d3d86f07c1cc94f80614
Lemarronier <hippolyte.pik@epita.fr>
2024-05-26 17:20:22 +0000

    front: init(selectionRect): add selection rect on map editor
    Squashed commit of the following:
    
    * front: fix(selection-rect): remove selection rect in edition
    
    * front: fix(selection-rect): the bug
    
    * front: fix(selection-rect): apply review but not the initial case which are still buggy
    
    * front: init(selectionRect): add selection rect on map editor

--- gui/godot/Scripts/Scenes/MapEditor/BGSelector.gd
+++ gui/godot/Scripts/Scenes/MapEditor/BGSelector.gd
@@ -3,10 +3,11 @@ extends TileMap
 
 signal on_selection(tile)
 
+onready var select_rect : Node2D = $"../../SelectionRect"
 
 func _ready():
 	var viewer : Viewer = get_tree().current_scene.find_node("Viewer")
-
+	
 	clear()
 
 	# Put item list
@@ -17,11 +18,16 @@ func _ready():
 		[Constants.TypeCase.OEUF_VERT, Constants.TypeCase.OEUF_BLEU, Constants.TypeCase.OEUF_ROUGE],
 		[Constants.TypeCase.OEUF_JAUNE, Constants.TypeCase.OEUF_GRIS],
 	]
-	for y in len(items):
+	for y in len(items): 
 		var row = items[y]
 		for x in len(row):
 			set_cell(x, y, viewer.case2tile[row[x]])
 
+func set_selection_rect(tilepos):
+	var pos : Vector2 = to_global(map_to_world(Vector2(tilepos[0], tilepos[1])))
+	select_rect.position.x = pos.x - 3
+	select_rect.position.y = pos.y - 3
+	select_rect.visible = true
 
 func _input(event):
 	if event is InputEventMouseButton and event.pressed:
@@ -29,4 +35,5 @@ func _input(event):
 		var tilepos = self.world_to_map(pos)
 		var tile = self.get_cell(tilepos[0], tilepos[1])
 		if tile != -1:
+			set_selection_rect(tilepos)
 			emit_signal("on_selection", tile)
