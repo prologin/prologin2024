8c3a7e4536326e6261e37c82b1e213aeb8650c32
Simon Scatton <simon.scatton@outlook.fr>
2024-05-30 23:41:11 +0000

    feat(nix): added game rst api generation in flake
    Squashed commit of the following:
    
    * feat(nix): added game rst api generation in flake

--- flake.nix
+++ flake.nix
@@ -30,6 +30,26 @@
 
           prologin2024-gui-html = final.callPackage ./gui/html.nix { html-export-templates =  final.html-export-templates; };
 
+          prologin2024-api-rst = final.stdenv.mkDerivation {
+            name = "prologin2024-api-rst";
+
+            src = ./.;
+
+            buildInputs = [ final.stechec2 ];
+
+            configurePhase = ''
+              ${final.stechec2}/bin/stechec2-generator sphinxdoc $src/prologin2024.yml .
+            '';
+
+            installPhase = ''
+              mkdir -p $out/
+              mv api.rst $out/api.rst
+            '';
+          };
+
+
+
+
         };
 
       };
@@ -43,7 +63,7 @@
 
         in
         rec {
-          packages = { inherit (pkgs) prologin2024 prologin2024-gui-linux prologin2024-gui-html; };
+          packages = { inherit (pkgs) prologin2024 prologin2024-gui-linux prologin2024-gui-html prologin2024-api-rst; };
 
           defaultPackage = self.packages.${system}.prologin2024;
 
@@ -57,4 +77,3 @@
     in
     recursiveUpdate anySystemOutputs multipleSystemsOutpus;
 }
-
