64419ace15907ce751ce545884b728e6c3e9da50
Paul Dufour <paul.paul.dufour@gmail.com>
2024-05-31 07:45:54 +0000

    frontend: add light theme
    Squashed commit of the following:
    
    * Apply 1 suggestion(s) to 1 file(s)
    
    Co-authored-by: Célian Raimbault <celian.dev@gmail.com>
    
    * feat(www): light theme default
    
    Signed-off-by: Paul Dufour <paul.dufour@prologin.org>
    
    * frontend: add light theme
    
    Signed-off-by: Paul Dufour <paul.dufour@prologin.org>

--- www/static/js/header.js
+++ www/static/js/header.js
@@ -1,4 +1,30 @@
+function setThemeDark() {
+  localStorage.theme = "dark";
+  $("body").toggleClass("light");
+}
+
+function setThemeLight() {
+  localStorage.theme = "light";
+  $("body").toggleClass("light");
+}
+
+function changeTheme() {
+  if (localStorage.theme == "dark") {
+    setThemeLight();
+  } else {
+    setThemeDark();
+  }
+}
+
 $(function () {
+  if (localStorage.theme == "light") {
+    setThemeLight();
+  }
+  $(".navbar-nav")
+    .append("<li><a>Change theme</a></li>")
+    .click(function () {
+      changeTheme();
+    });
   $("[role=complementary] h1")
     .text("")
     .html('<img src="/static/img/Prologin2024.png" style="width:100%;" />');
